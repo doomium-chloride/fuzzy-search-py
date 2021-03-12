9867f6d702316a6fcf5e68683aa1c6c219b351bb
doomium-chloride <47913762+doomium-chloride@users.noreply.github.com>
2021-03-13 01:06:36 +1300

    Change setup

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
 import setuptools
 
-setuptools.setup()
+setuptools.setup(
+    name='FuzzySearchPy',
+    version='0.0.2',
+    packages=setuptools.find_packages(exclude=['tests*']),
+    license='WTFPL',
+    description='A port of FuzzySearch from JS',
+    long_description=open('README.txt').read(),
+    install_requires=['numpy'],
+    url='https://github.com/doomium-chloride/fuzzy-search-py',
+    author='doomium-chloride'
+)
