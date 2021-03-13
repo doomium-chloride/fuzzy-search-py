e3f41c43a123eb1e2e9c5f817a64332f21ebca42
doomium-chloride <47913762+doomium-chloride@users.noreply.github.com>
2021-03-13 15:17:31 +1300

    Remove entry point

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='FuzzySearchPy',
-    version='0.0.6',
+    version='0.0.7',
     package_dir={"": "FuzzySearchPy"},
     packages=setuptools.find_packages(where="FuzzySearchPy"),
     license='WTFPL',
@@ -10,10 +10,5 @@ setuptools.setup(
     long_description=open('readme.md').read(),
     url='https://github.com/doomium-chloride/fuzzy-search-py',
     author='doomium-chloride',
-    author_email='evilsmellingliquid@gmail.com',
-    entry_points={
-        "console_scripts": [
-            "FuzzySearchPy=FuzzySearchPy.FuzzySeach:FuzzySearch"
-        ]
-    }
+    author_email='evilsmellingliquid@gmail.com'
 )
